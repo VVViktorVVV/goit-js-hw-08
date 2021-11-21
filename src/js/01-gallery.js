62db5b5150201f9a3b51b160da327320438b342f
Viktor Bondar <vktrbndr@gmail.com>
2021-11-21 23:44:31 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+
+
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryBox = document.querySelector('.gallery');
+
+const createGalleryIteam = function (array) {
+    return array.map(({ description, original, preview }) => {
+        return `<div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </div>`
+    }).join('');
+    
+}
+
+const gallery = createGalleryIteam(galleryItems);
+
+galleryBox.insertAdjacentHTML('beforeend', gallery);
+
+new SimpleLightbox('.gallery a', {
+    captionsType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+galleryBox.addEventListener('click', showOriginalImage);
+
+function showOriginalImage(evn) {
+    evn.preventDefault();
+
+    if (!evn.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+
+    
+
+}
