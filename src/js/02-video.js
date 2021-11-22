24cb4eaa4778c7b3a6d1852cd86a0bfb2dfbd74a
Viktor Bondar <vktrbndr@gmail.com>
2021-11-22 02:12:03 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+    console.log('title:', title);
+});
+
+
+player.on('timeupdate', throttle(function (event) {
+    const time = event.seconds;
+    localStorage.setItem("videoplayer-current-time", time);
+    console.log(time);
+}, 1000));
+
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
 
